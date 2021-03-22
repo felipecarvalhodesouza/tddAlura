b5ae80866eade6328c23f009236a96656eecd242
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2021-03-22 18:54:28 -0300

    Começando com testes de unidade

---
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -0,0 +1,33 @@
+package br.com.caelum.leilao.servico;
+
+import java.util.List;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+
+public class Avaliador {
+
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+	public void avalia(Leilao leilao) {
+
+		List<Lance> lances = leilao.getLances();
+		for (Lance lance : lances) {
+			if (lance.getValor() > maiorDeTodos) {
+				maiorDeTodos = lance.getValor();
+			}
+			if (lance.getValor() < menorDeTodos) {
+				menorDeTodos = lance.getValor();
+			}
+		}
+	}
+
+	public double getMaiorLance() {
+		return maiorDeTodos;
+	}
+
+	public double getMenorLance() {
+		return menorDeTodos;
+	}
+}
